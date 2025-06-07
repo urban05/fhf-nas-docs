85a39eafaa58fd62cfc08d6d083baa2d77aa8cc7
Doran Urban <kontakt@doranurban.de>
2025-06-07 11:10:13 +0200

    Create discord_notify.sh

---
+++ discord_notify.sh
@@ -0,0 +1,7 @@
+WEBHOOK_URL="https://discord.com/api/webhooks/[URL]"
+MESSAGE=$1
+
+curl -H "Content-Type: application/json" \
+     -X POST \
+     -d "{\"content\": \"$MESSAGE\"}" \
+     $WEBHOOK_URL
